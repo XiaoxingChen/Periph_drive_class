6b091490c240a061e9453854103b83d7c7fc9083
XiaoxingChen <cxx@seer-robotics.com>
2016-01-29 20:59:13 +0800

    chenxx 2016/01/29
    Adjusted CDma_Base and CInte_Base to a abstract base class. Peripheral classes call DMA and INTE functions with pointer and virtual function.
    Give up the old form of CPeriph. Keep only one virtual function that called by CDma_Base.
    Give up almost all the template classes.

--- CPeriph.h
+++ CPeriph.h
@@ -2,9 +2,9 @@
   ******************************************************************************
   * @file    CPeriph.h
   * @author  Chenxx
-  * @version V1.1
+  * @version V1.2
   * @date    2015-09-05
-  * @brief   This file defines the class CInte_Base, CDma_Base, CInte_Dma, CPeriph.
+  * @brief   This file defines the class CInte_Base, CDma_Base, CInte_Dma.
   ******************************************************************************/
 #ifndef __CPERIPH_HPP
 #define __CPERIPH_HPP
@@ -20,18 +20,22 @@ class CInte_Base
 {
 public:
 	CInte_Base()
-		:NVIC_IRQChannel_(0)
-	{}
-	CInte_Base(uint8_t NVIC_IRQChannel)
-		:NVIC_IRQChannel_(NVIC_IRQChannel)
 	{}
 	uint8_t GetIRQChannel(void)	
-		{return NVIC_IRQChannel_;}
-	~CInte_Base(void)
+	{
+		return NVIC_IRQChannel_;
+	}
+	void Init(uint8_t PrePriority, uint8_t SubPriority);
+	void Init(uint8_t PrePriority, uint8_t SubPriority, uint32_t Periph_IT);
+	virtual void EnableLine(uint32_t Periph_IT) = 0;
+	virtual void DisableLine(uint32_t Periph_IT) = 0;
+		
+	virtual ~CInte_Base(void)
 	{}
 protected:
 	void NvicConfig(uint8_t PrePriority,uint8_t SubPriority);
 	uint8_t NVIC_IRQChannel_;	
+	void* Periphx_;
 };
 
 /******************************************************************************
@@ -41,26 +45,14 @@ class CInte_Dma
 	:public CInte_Base
 {
 public:
-	CInte_Dma(const CDma_Base& ODma);
-	inline void Init(uint8_t PrePriority, uint8_t SubPriority)
-	{
-		NvicConfig(PrePriority, SubPriority);
-	}
-	inline void Init(uint8_t PrePriority, uint8_t SubPriority, uint32_t DMA_IT)
-	{
-		NvicConfig(PrePriority, SubPriority);
-		EnableLine(DMA_IT);
-	}
-	void EnableLine(uint32_t DMA_IT)
-		{DMA_ITConfig(DMAy_Channelx_ ,DMA_IT,ENABLE);}	
-	void DisableLine(uint32_t DMA_IT)
-		{DMA_ITConfig(DMAy_Channelx_ ,DMA_IT,DISABLE);}
+	CInte_Dma(const CDma_Base* pDma);
+	virtual void EnableLine(uint32_t DMA_IT)
+		{DMA_ITConfig((DMA_Channel_TypeDef*)Periphx_ ,DMA_IT,ENABLE);}	
+	virtual void DisableLine(uint32_t DMA_IT)
+		{DMA_ITConfig((DMA_Channel_TypeDef*)Periphx_ ,DMA_IT,DISABLE);}
 		
-	DMA_Channel_TypeDef* GetDMAy_Channelx_(void)
-		{return DMAy_Channelx_;}
-		
-private:
-	DMA_Channel_TypeDef* DMAy_Channelx_;
+//	DMA_Channel_TypeDef* GetDMAy_Channelx_(void)
+//		{return (DMA_Channel_TypeDef*)Periphx_;}
 };
 
 /******************************************************************************
@@ -69,12 +61,7 @@ private:
 class CDma_Base
 {
 public:
-	
-	CDma_Base():INTE(NULL), DMAy_Channelx_(0), NVIC_IRQChannel_(0) 
-	{
-		memset(&DMA_InitStructure_, 0, sizeof(DMA_InitStructure_));
-	}
-	CDma_Base(DMA_Channel_TypeDef* DMAy_Channelx);
+	CDma_Base(DMA_Channel_TypeDef* DMAy_Channelx = 0);
 	void ClkConfig(void);
 	void Config(uint32_t PeriphAddr,
 							uint32_t MemAddr,
@@ -83,32 +70,13 @@ public:
 							uint32_t PDataSize,	
 							uint32_t MDataSize,
 							uint32_t DMA_Mode);
+	
 	inline void Enable(void)
 		{DMA_Cmd(DMAy_Channelx_, ENABLE);}
 	inline void Disable(void)
 		{DMA_Cmd(DMAy_Channelx_, DISABLE);}
-	inline void Reboot(uint32_t ReloadMemAddr, u16 ReloadBufferSize)
-	{
-		Disable();
-		
-		if(ReloadBufferSize != 0)
-		{
-			DMAy_Channelx_->CNDTR = ReloadBufferSize;
-			DMA_InitStructure_.DMA_BufferSize = ReloadBufferSize;
-		}
-		else	//ReloadBufferSize == 0, load the size that used last time
-			DMAy_Channelx_->CNDTR = DMA_InitStructure_.DMA_BufferSize;
-		
-		if(ReloadMemAddr != NULL)
-		{
-			DMAy_Channelx_->CMAR = ReloadMemAddr;
-			DMA_InitStructure_.DMA_MemoryBaseAddr = ReloadMemAddr;
-		}
-		else
-			DMAy_Channelx_->CMAR = DMA_InitStructure_.DMA_MemoryBaseAddr;
-		
-		Enable();
-	}
+	void Reboot(uint32_t ReloadMemAddr, u16 ReloadBufferSize);
+	
 	CInte_Dma* INTE;
 	
 	DMA_Channel_TypeDef* GetDMAy_Channelx_(void) const
@@ -121,57 +89,64 @@ public:
 			return DMA_InitStructure_;
 		else while(1); //#error! Value was not set
 	}
-	~CDma_Base(void)
-		{/*delete INTE;*/}
+	
+	virtual void PeriphConfig() = 0;
+	virtual void Init(uint32_t PeriphAddr,
+		uint32_t MemAddr,
+		uint32_t DMA_DIR,
+		uint32_t BufferSize,
+		uint32_t PDataSize,	
+		uint32_t MDataSize,
+		uint32_t DMA_Mode,
+		CInte_Dma* pInte_Dma) = 0;
+
+	virtual ~CDma_Base(void)
+	{
+		delete INTE;
+	}
 protected:
 	DMA_Channel_TypeDef* DMAy_Channelx_;
 	DMA_InitTypeDef DMA_InitStructure_;
 	uint8_t NVIC_IRQChannel_;
 };
 /******************************************************************************
+* @brief   This part for CPeriph
+******************************************************************************/
+class CPeriph
+{
+public:
+	CPeriph()
+	{}
+	virtual CDma_Base* Creat_CDma(int DmaCh) const = 0;
+	static void EmptyInit();
+	virtual ~CPeriph()
+	{}
+	
+private:
+};
+/******************************************************************************
 * @brief   This part for CDmaKit
 ******************************************************************************/
-template <typename TDma_Periph, typename TPeriph>
-class CDmaKit
-	:public kit<TDma_Periph>
+class CDmaKit:
+	public kit<CDma_Base*>
 {
-public:		
-	CDmaKit(const TPeriph& OPeriph)
-		:OPeriph_(OPeriph)
+public:
+	CDmaKit(const CPeriph* pPeriph):
+		pPeriph_(pPeriph)
 	{}
 	~CDmaKit()
 	{}
-	void add(typename TPeriph::DmaChType DmaCh)
+		
+	void add(int DmaCh)
 	{
-		kit<TDma_Periph>::registry_.push_back(
-			typename kit<TDma_Periph>::Association(DmaCh, TDma_Periph(OPeriph_.DmaChCalc(DmaCh))));
-	}		
-protected:
-	const TPeriph& OPeriph_;
-};
-/******************************************************************************
-* @brief   This part for CPeriph
-******************************************************************************/
-template <typename TInte, typename TDma_Periph, typename TPeriph>
-//template <typename TInte, typename T2>
-class CPeriph 
-{
-public:
-	CPeriph(void (*InitFunc)(void))
-		:Init(InitFunc),INTE(NULL),DMA(NULL)
-	{
-		//InitFunc is a pointer, but was passed to base class
+		kit::add(DmaCh, pPeriph_->Creat_CDma(DmaCh));
 	}
-	void (*Init)(void);
-	TInte* INTE;
-	CDmaKit<TDma_Periph, TPeriph>* DMA;
-	~CPeriph(void)
+	void destroy(int DmaCh)
 	{
-		delete INTE;
-		delete DMA;
+		delete handle(DmaCh);
+		while(1);// TO DO. The 'destroy' in "kit.h" was left unfinished!
 	}
-protected:
-
+private:
+	const CPeriph* pPeriph_;
 };
-
 #endif 
