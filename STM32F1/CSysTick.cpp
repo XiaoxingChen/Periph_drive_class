ec277e7d710f7e7cef786839674f2e8e2004806a
XiaoxingChen <cxx@seer-robotics.com>
2016-07-21 09:59:41 +0800

    chenxx add task period module

--- STM32F1/CSysTick.cpp
+++ STM32F1/CSysTick.cpp
@@ -9,6 +9,13 @@
 #include "CSysTick.h"
 #include "stddef.h"
 
+//const uint16_t CSysTick::taskPeriodOverflowNbr[CSysTick::NUM_OF_TASK_PERIOD] = 
+//{1, 2, 5, 10, 20, 50, 100, 200, 5000, 10000};
+const uint16_t CSysTick::taskPeriodOverflowNbr[CSysTick::NUM_OF_TASK_PERIOD] = 
+{0, 0, 0, 1, 2, 5, 10, 20, 500, 1000};
+uint16_t CSysTick::taskPeriodCounter[CSysTick::NUM_OF_TASK_PERIOD];
+uint16_t CSysTick::missUpCounter_ = 0;
+uint32_t CSysTick::absoluteTime_ = 0;
 //
 //constructor
 //
@@ -42,5 +49,58 @@ uint32_t CSysTick::get_Freq()
 {
 	uint32_t freq = 9000000/SysTick->LOAD;
 	return freq;
+}
+
+/**
+  * @brief  get the system absolute run time
+  * @param  None
+  * @retval absoluteTime_
+  */
+uint32_t CSysTick::getAbsoluteTime()
+{
+	return absoluteTime_;
+}
+
+/**
+  * @brief  constructor
+  * @param  timerPeriod
+  * @retval None
+  */
+CTickTimer::CTickTimer(uint32_t timerPeriod)
+	:timerPeriod_(timerPeriod)	
+{}
+	
+/**
+  * @brief  reset and start the timer
+  * @param  None
+  * @retval None
+  */
+void CTickTimer::restart()
+{
+	absoluteStartTime_ = sysTick::Instance()->getAbsoluteTime();
+}
+
+/**
+  * @brief  reset and start the timer
+  * @param  None
+  * @retval None
+  */
+bool CTickTimer::isTimeUp()
+{
+	uint32_t absoluteTime = sysTick::Instance()->getAbsoluteTime();
+	uint32_t targetTime = absoluteStartTime_ + timerPeriod_;
+	if(targetTime > absoluteStartTime_)
+	{
+		if((absoluteTime >= absoluteStartTime_) && (absoluteTime < targetTime)) 
+			return false;
+		else 
+			return true;
+	}else //overflow
+	{
+		if((absoluteTime >= targetTime) && (absoluteTime < absoluteStartTime_))
+			return true;
+		else
+			return false;
+	}
 }
 //end of file
--- STM32F1/CSysTick.h
+++ STM32F1/CSysTick.h
@@ -10,10 +10,27 @@
 #define __CSYSTICK_H
 #include "stm32f10x.h"
 #include "singleton.h"
+
 class CSysTick
 {	
 public:
 	CSysTick();
+	enum taskPeriod
+	{
+		TASK_100US = 0,
+		TASK_200US,
+		TASK_500US,
+		TASK_1MS,
+		TASK_2MS,
+		TASK_5MS,
+		TASK_10MS,
+		TASK_20MS,
+		TASK_500MS,
+		TASK_1S,
+		NUM_OF_TASK_PERIOD //keep this at end of enum
+	};
+	static const uint16_t taskPeriodOverflowNbr[NUM_OF_TASK_PERIOD];
+	static uint16_t taskPeriodCounter[NUM_OF_TASK_PERIOD];
 	inline void reset()
 	{
 		SysTick->VAL = 0;
@@ -28,13 +45,49 @@ public:
 	}
 	inline void wait()
 	{
+		/* update taskPeriodCounter array */
+		for(int i = 1; i < NUM_OF_TASK_PERIOD; i++)
+		{
+			taskPeriodCounter[i]++;
+			if(taskPeriodCounter[i] >= taskPeriodOverflowNbr[i])
+			{
+				taskPeriodCounter[i] = 0;
+			}
+		}
+		
+		/* count absoluteTime_ */
+		absoluteTime_++;
+		
+		/* check if the cycle time is enough*/
+		if((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) != 0) missUpCounter_++;
+		
+		/* waiting */
 		while((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) == 0);
+
 	}
-	void SetFreq(uint32_t freq);
+	void SetFreq(uint32_t freq);//TODO: change to MACRO. 
 	static uint32_t get_Freq();
-private:
+	uint32_t getAbsoluteTime();
 	
+private:
+	static uint16_t missUpCounter_; 
+	static uint32_t absoluteTime_;
 };
+#define PERIOD_RUN(period, task) \
+if(!CSysTick::taskPeriodCounter[period]) task;
 typedef NormalSingleton<CSysTick> sysTick;
+
+//CTickTimer
+class CTickTimer
+{
+public:
+	CTickTimer(uint32_t);
+	bool isTimeUp();
+	void restart();
+
+private:
+	uint32_t absoluteStartTime_;
+	uint32_t timerPeriod_;
+};
 #endif
 //end of file
